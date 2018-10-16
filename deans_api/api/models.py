6ae963dd37ddb1635cd859e661a4459f17840c57
JQIANG125 <32261300+JQIANG125@users.noreply.github.com>
2018-10-16 15:47:38 +0800

    Update models.py

--- deans_api/api/models.py
+++ deans_api/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Operator(models.Model):
+	Operator_Id = models.IntegerField()
+	Operator_Password = models.CharField(max_length=20)
+	Operator_Name = models.CharField(max_length=30)
+	Is_Admin = models.BooleanField()
+	crisis = models.ManyToManyField('Crisis')
+	class Meta:
+		ordering =('-Operator_Id')
+    
+class Crisis(models.Model):
+	Crisis_Id = models.IntegerField()
+	Crisis_Category = models.CharField()
+	Crisis_Description = models.TextField()
+	Crisis_Assitance = models.CharField()
+	Crisis_Status = models.CharField()
+	Crisis_Time = models.DateTimeField(auto_now_add=True)
+	Crisis_Location = models.TextField()
+	class Meta:
+		ordering =('-Crisis_Id')
