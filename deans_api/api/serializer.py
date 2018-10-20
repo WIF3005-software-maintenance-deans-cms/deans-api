047667785c2b2c1e020c44885eca08a3d9cc8364
Xinye <xinyesg@gmail.com>
2018-10-21 01:01:04 +0800

    Can login and auth

--- deans_api/api/serializer.py
+++ deans_api/api/serializer.py
@@ -53,3 +53,4 @@ class UserSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = User
+		fields = ('id', 'username', 'password', 'is_staff')
--- deans_api/api/urls.py
+++ deans_api/api/urls.py
@@ -7,18 +7,17 @@ from .views import (
     CrisisViewSet,
     CrisisAssistanceViewSet,
     CrisisTypeViewSet,
-    UserCreateView
+    UserViewSet
 )
 
 router = routers.DefaultRouter()
 router.register(r'crises', CrisisViewSet)
 router.register(r'crisisassistance', CrisisAssistanceViewSet)
 router.register(r'crisistype', CrisisTypeViewSet)
+router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
 	path('api-auth/', include('rest_framework.urls')),
-	path('register/', UserCreateView.as_view(), name='user-register'),
-	# path('crises/', CrisisListAPIView.as_view(), name='user-list'),
     url(r'^', include(router.urls)),
 ]
