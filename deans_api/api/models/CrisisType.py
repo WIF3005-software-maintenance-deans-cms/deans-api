b0df351d78b65b8f4670381de89905035901eed2
Jiatai <JQIANG002@E.NTU.EDU.SG>
2018-10-23 20:19:41 +0800

    Fix

--- deans_api/api/models/CrisisType.py
+++ deans_api/api/models/CrisisType.py
@@ -2,9 +2,10 @@ from django.db import models
     
     
 class CrisisType (models.Model):
-	name=models.CharField(
+	name=models.ListCharField(base_field=CharField(max_length=20),
+			size=20,
 			default=None,
-			max_length=255)
+			max_length=(20*20))
 	
 	def __str__(self):
 		return self.name
