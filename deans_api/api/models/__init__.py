0960498d1dd64c094a3272fed2fff1d3fb2ed122
Jiatai <JQIANG002@E.NTU.EDU.SG>
2018-10-23 17:54:39 +0800

    Fix

--- deans_api/api/models/__init__.py
+++ deans_api/api/models/__init__.py
@@ -3,3 +3,6 @@ from .CrisisType import CrisisType
 from .Operator import Operator
 from .CrisisAssistance import CrisisAssistance
 from .SiteSettings import SiteSettings
+from .EmergencyAgencies import EmergencyAgencies
+from .SocialMediaAccount import SocialMediaAccount
+from .SiteSettings import SiteSettings
