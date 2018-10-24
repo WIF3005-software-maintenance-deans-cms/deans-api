f20b460d399958633e0d41fd0cd7e918beb1746f
Xinye <xinyesg@gmail.com>
2018-10-24 20:52:31 +0800

    fix typo

--- start_django.sh
+++ start_django.sh
@@ -3,6 +3,6 @@ cd $DJANGO_ROOT
 python3 manage.py collectstatic --no-input;
 python3 manage.py makemigrations;
 python3 manage.py migrate;
-python manage.py loaddata $DATA_ROOT/users.json
+python3 manage.py loaddata $DATA_ROOT/users.json
 python3 manage.py runserver 0.0.0.0:8000;
 # gunicorn  --log-level=debug --bind :8000 deans_api.wsgi:application;
