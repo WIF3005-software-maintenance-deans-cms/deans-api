d3a51adc2e21ff5edb96fbf9773d836b59dec490
Xinye <xinyesg@gmail.com>
2018-10-25 14:03:53 +0800

    add api prefix

--- deans_api/deans_api/urls.py
+++ deans_api/deans_api/urls.py
@@ -39,7 +39,7 @@ router.register(r'users', UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-	path('admin/', admin.site.urls),
+	path('api/admin/', admin.site.urls),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include('api.urls')),
+    url(r'^api/', include('api.urls')),
 ]
