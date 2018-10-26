f55bb4638b5736f3b262b2f3c90a673ea21f5f6c
Jiatai <JQIANG002@E.NTU.EDU.SG>
2018-10-26 14:34:03 +0800

    fix

--- deans_api/api/models/CrisisType.py
+++ deans_api/api/models/CrisisType.py
@@ -3,7 +3,7 @@ from django_mysql.models import ListCharField
     
 class CrisisType (models.Model):
 	name=models.ListCharField(
-			base_field=CharField(max_length=30)
+			base_field=CharField(max_length=30),
 			default=None,
 			size=10,
 			max_length=(10*30))
