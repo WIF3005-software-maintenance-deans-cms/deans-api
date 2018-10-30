69c9d58bd6252fd136847defef2a3fecd3125c52
yc <yuancez@gmail.com>
2018-10-30 22:17:28 +0800

    Dispatch

--- deans_api/api/models/Crisis.py
+++ deans_api/api/models/Crisis.py
@@ -3,35 +3,45 @@ from django.db import models
 from .CrisisType import CrisisType
 from .Operator import Operator
 from .CrisisAssistance import CrisisAssistance
+from django.db.models import signals
+import requests
 
 STATUS_CHOICES = (
-	('PD', 'Pending'),
-	('DP', 'Dispatched'),
-	('RS', 'Resolved'),
+    ('PD', 'Pending'),
+    ('DP', 'Dispatched'),
+    ('RS', 'Resolved'),
 )
 
 class Crisis(models.Model):
-	your_name = models.CharField(default=None,max_length=255)
-	mobile_number = models.CharField(default=None,max_length=255)
-	crisis_id = models.AutoField(primary_key=True)
-	crisis_type = models.ManyToManyField(CrisisType)
-	crisis_description = models.TextField()
-	crisis_assistance = models.ManyToManyField(CrisisAssistance)
-	crisis_assistance_description = models.TextField()
-	crisis_time = models.DateTimeField(auto_now_add=True)
-	crisis_location1 = models.TextField()
-	crisis_location2 = models.TextField()
-	updated_at = models.DateTimeField(auto_now_add=True)
-	owner = models.ManyToManyField(User)
-	visible = models.BooleanField(default=True) 
-	# TODO: support visible in backend
+    your_name = models.CharField(default=None,max_length=255)
+    mobile_number = models.CharField(default=None,max_length=255)
+    crisis_id = models.AutoField(primary_key=True)
+    crisis_type = models.ManyToManyField(CrisisType)
+    crisis_description = models.TextField()
+    crisis_assistance = models.ManyToManyField(CrisisAssistance)
+    crisis_assistance_description = models.TextField()
+    crisis_time = models.DateTimeField(auto_now_add=True)
+    crisis_location1 = models.TextField()
+    crisis_location2 = models.TextField()
+    updated_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ManyToManyField(User)
+    visible = models.BooleanField(default=True)
 
-	crisis_status = models.CharField(choices=STATUS_CHOICES, default='PD',  max_length=254)
+    dispatch_trigger = models.BooleanField(default=False)
+    # TODO: support visible in backend
 
-	def __str__(self):
-		return str(self.crisis_id)
+    crisis_status = models.CharField(choices=STATUS_CHOICES, default='PD',  max_length=254)
 
-	class Meta:
-		ordering = ['-crisis_id']
+    def __str__(self):
+        return str(self.crisis_id)
 
+    class Meta:
+        ordering = ['-crisis_id']
+
+def dispatch_trigger(sender, instance, created, **kwargs):
+
+    if sender.dispatch_trigger:
+        requests.post("http://notification:8000/dispatchnotices/", data={"number" : "+6586963013", "message" : "go go go, Fire"})
+
+signals.post_save.connect(receiver=dispatch_trigger, sender=Crisis)
 
